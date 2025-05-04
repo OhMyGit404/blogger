24acb95ca8f9f40031ad44c6e53655d765523d12
404UsernameNotFound <allan.ng'ang'a@strathmore.edu>
2025-05-05 00:37:19 +0300

    added pagination and filtering by authors

--- MyBlog/urls.py
+++ MyBlog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from django.contrib import admin
 urlpatterns = [
     path('',PostListView.as_view(), name='index'),
+    path('user/<str:username>/', UserPostListView.as_view(), name='user_posts'),
     path('post/<int:pk>/',PostDetailView.as_view(), name='post_detail'),
     path('post/new/', PostCreateView.as_view(), name='post_create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
--- MyBlog/views.py
+++ MyBlog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from MyBlog.models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
@@ -14,7 +15,18 @@ class PostListView(ListView):
     template_name = 'Blog/index.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
-   # paginate_by = 5
+    paginate_by = 5
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'Blog/user_posts.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model = Post
