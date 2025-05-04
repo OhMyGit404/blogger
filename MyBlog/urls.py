2a6b106ff38a6ae35c369eab02a97ba9e5b1f7a4
404UsernameNotFound <allan.ng'ang'a@strathmore.edu>
2025-05-04 22:32:10 +0300

    added listview and detailview

--- MyBlog/urls.py
+++ MyBlog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView
 from django.contrib import admin
 urlpatterns = [
-    path('',views.index, name='index'),
+    path('',PostListView.as_view(), name='index'),
+    path('post/<int:pk>/',PostDetailView.as_view(), name='post_detail'),
     path('about/',views.about, name='about'),
     path('admin/', admin.site.urls),
 ]
--- MyBlog/views.py
+++ MyBlog/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from MyBlog.models import Post
-
 
 def index(request):
     context = {
         'posts': Post.objects.all(),
     }
     return render(request, 'Blog/index.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'Blog/index.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 5
+
+class PostDetailView(DetailView):
+    model = Post
+
+"""
+class PostCreateView(CreateView):
+    model = Post
+"""
+
 
 def about(request):
     return render(request, 'Blog/about.html', {'title': 'About'})
